afeb39aa132593ca17c65fef561db72348d20a63
bvegam <vegabryam40@gmail.com>
2025-04-23 15:55:22 -0500

    init structure project

---
+++ src/test/java/com/cat/practice/rest/PracticeRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cat.practice.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
